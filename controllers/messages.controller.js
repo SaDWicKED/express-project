3b8da41aa09a7bc756a030b1ded1f8c331a4c6fe
Silvio Sol <sadwick1989@gmail.com>
2022-02-16 16:46:23 -0500

    Implemented route to serve static files

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,9 +1,8 @@
 const path = require('path');
 
 function getMessages(req, res) {
-  const filePath = path.join(__dirname, '..', 'public', 'skimountain.jpg');
+  const filePath = path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg');
   res.sendFile(filePath);
-  // res.send('<ul><li>Hello Albert!</li></ul>');
 }
 
 function postMessage(req, res) {
